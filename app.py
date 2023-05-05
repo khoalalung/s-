23afb7d1d865aa588829dfc21ebffd625e08c8a2
dorime <73388076+gsthong@users.noreply.github.com>
2023-05-05 19:19:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,35 +60,35 @@ def detect_face():
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             
-                    # Xác định vị trí khuôn mặt
-        face_roi = frame[y:y+h, x:x+w]
-        
-        # Đưa khuôn mặt qua mô hình để phân loại giới tính
-        blob = cv2.dnn.blobFromImage(face_roi, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
-        gender_net.setInput(blob)
-        gender_preds = gender_net.forward()
-        
-        # Đưa khuôn mặt qua mô hình để phân loại độ tuổi
-        age_net.setInput(blob)
-        age_preds = age_net.forward()
-        age = age_preds[0].dot(np.arange(0, 101).reshape(101, 1)).flatten()[0]
-        
-        # Xác định giới tính dựa trên kết quả phân loại
-        gender = "Male" if gender_preds[0][0] > 0.5 else "Female"
-        
-        # Hiển thị thông tin khuôn mặt
+             # Xác định vị trí khuôn mặt
+            face_roi = frame[y:y+h, x:x+w]
+
+            # Đưa khuôn mặt qua mô hình để phân loại giới tính
+            blob = cv2.dnn.blobFromImage(face_roi, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+            gender_net.setInput(blob)
+            gender_preds = gender_net.forward()
+
+            # Đưa khuôn mặt qua mô hình để phân loại độ tuổi
+            age_net.setInput(blob)
+            age_preds = age_net.forward()
+            age = age_preds[0].dot(np.arange(0, 101).reshape(101, 1)).flatten()[0]
+
+            # Xác định giới tính dựa trên kết quả phân loại
+            gender = "Male" if gender_preds[0][0] > 0.5 else "Female"
+
+            # Hiển thị thông tin khuôn mặt
         st.write(f"Age: {int(age)}")
         st.write(f"Gender: {gender}")
         confidence = np.max(gender_preds)
         st.write(f"Confidence: {confidence}")
-    
-    # Hiển thị hình ảnh
+
+        # Hiển thị hình ảnh
     st.image(frame, channels="BGR")
-    
-    # Kiểm tra người dùng có muốn dừng chương trình hay không
+
+            # Kiểm tra người dùng có muốn dừng chương trình hay không
     if st.button("Stop"):
-        break
-    
+      break
+
 # Giải phóng tài nguyên
 cap.release()
 cv2.destroyAllWindows()
