8340d109a8f819130d0a8154a4e81c443094fd0f
dorime <73388076+gsthong@users.noreply.github.com>
2023-05-05 19:46:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,102 +1,65 @@
-import streamlit as st
 import cv2
-import numpy as np
-import os
-
-# Phần 1: Chụp hình và ghi thông tin
+import streamlit as st
 
 def capture_and_register():
-    # Khởi tạo camera
+    global cap
     cap = cv2.VideoCapture(0)
-    
-    # Lấy hình ảnh từ camera
-    ret, frame = cap.read()
-    
-    # Hiển thị hình ảnh
-    st.image(frame, channels="BGR")
-    
-    # Nhập thông tin người dùng
-    name = st.text_input("Name:")
-    age = st.number_input("Age:")
-    gender = st.selectbox("Gender:", ["Male", "Female"])
-    
-    # Nếu thông tin không đầy đủ, yêu cầu nhập lại
-    if name == "" or age == 0 or gender == "":
-        st.warning("Please fill in all required information.")
-        return
-    
-    # Tạo thư mục để lưu thông tin
-    if not os.path.exists("data"):
-        os.makedirs("data")
-    
-    # Lưu thông tin vào file
-    filename = f"data/{name}.txt"
-    with open(filename, "w") as f:
-        f.write(f"Name: {name}\n")
-        f.write(f"Age: {age}\n")
-        f.write(f"Gender: {gender}\n")
-    st.success("Registration done!")
-
-# Phần 2: Nhận diện khuôn mặt và hiển thị thông tin
-
-def detect_face():
-    # Khởi tạo camera
-    cap = cv2.VideoCapture(0)
-    
-    # Đọc mô hình nhận diện khuôn mặt và phân loại giới tính
-    face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    age_net = cv2.dnn.readNetFromCaffe("age.prototxt", "age.caffemodel")
-    gender_net = cv2.dnn.readNetFromCaffe("gender.prototxt", "gender.caffemodel")
-    
+    st.write("Hi, let's register your face")
     while True:
-        # Lấy hình ảnh từ camera
         ret, frame = cap.read()
-        
-        # Nhận diện khuôn mặt
+        if not ret:
+            st.warning("Could not read from the camera")
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        st.image(frame, use_column_width=True, channels="RGB")
+        if st.button("Register"):
+            cv2.imwrite("registered_face.jpg", frame)
+            st.success("Successfully registered your face!")
+            break
+    cap.release()
+
+def detect():
+    st.write("Face detection")
+    registered_face = cv2.imread("registered_face.jpg")
+    if registered_face is None:
+        st.warning("No registered face found. Please register your face first.")
+        return
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.warning("Could not read from the camera")
+            break
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_detector.detectMultiScale(gray, 1.3, 5)
-        
-        # Hiển thị các khuôn mặt được nhận diện
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            
-             # Xác định vị trí khuôn mặt
-            face_roi = frame[y:y+h, x:x+w]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+            try:
+                res = cv2.matchTemplate(roi_gray, registered_face, cv2.TM_CCOEFF_NORMED)
+                if res > 0.8:
+                    st.warning("Unlocked. Face recognized.")
+                else:
+                    st.warning("Face not recognized.")
+            except:
+                pass
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        st.image(frame, use_column_width=True, channels="RGB")
+        if st.button("Stop"):
+            break
+    cap.release()
 
-            # Đưa khuôn mặt qua mô hình để phân loại giới tính
-            blob = cv2.dnn.blobFromImage(face_roi, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
-            gender_net.setInput(blob)
-            gender_preds = gender_net.forward()
+if __name__ == '__main__':
+    st.title("Face Recognition")
+    st.sidebar.title("Menu")
+    app_mode = st.sidebar.selectbox("Choose the app mode", ["Homepage", "Register", "Detect"])
 
-            # Đưa khuôn mặt qua mô hình để phân loại độ tuổi
-            age_net.setInput(blob)
-            age_preds = age_net.forward()
-            age = age_preds[0].dot(np.arange(0, 101).reshape(101, 1)).flatten()[0]
-
-            # Xác định giới tính dựa trên kết quả phân loại
-            gender = "Male" if gender_preds[0][0] > 0.5 else "Female"
-
-            # Hiển thị thông tin khuôn mặt
-            st.write(f"Age: {int(age)}")
-            st.write(f"Gender: {gender}")
-            confidence = np.max(gender_preds)
-            st.write(f"Confidence: {confidence}")
-
-            # Hiển thị hình ảnh
-            st.image(frame, channels="BGR")
-
-            # Kiểm tra người dùng có muốn dừng chương trình hay không
-            if st.button("Stop"):
-                cap.release()
-                cv2.destroyAllWindows()
-                return
-
-# Chạy chương trình
-if st.button("Register"):
-    capture_and_register()
-if st.button("Detect"):
-    detect_face()
-
-# Giải phóng tài nguyên
-# cap.release()
-# cv2.destroyAllWindows()
+    if app_mode == "Homepage":
+        st.write("Welcome to Face Recognition")
+        st.write("Please select a mode from the menu.")
+    elif app_mode == "Register":
+        capture_and_register()
+    elif app_mode == "Detect":
+        detect()
