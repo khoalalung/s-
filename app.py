fc2255d3a974fe0989039710d51ee60252f10856
dorime <73388076+gsthong@users.noreply.github.com>
2023-05-05 20:40:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# # import cv2
-# # import streamlit as st
+import cv2
+import streamlit as st
 
 # # def capture_and_register():
 # #     global cap
@@ -68,70 +68,68 @@
 # import cv2
 # import streamlit as st
 
-# def capture_and_register():
-#     global cap
-#     cap = cv2.VideoCapture(0)
-#     st.write("Hi, let's register your face")
-#     while True:
-#         ret, frame = cap.read()
-#         if not ret:
-#             st.warning("Could not read from the camera. Please try again.")
-#             break
-#         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         st.image(frame, use_column_width=True, channels="RGB")
-#         if st.button("Register"):
-#             try:
-#                 cv2.imwrite("registered_face.jpg", frame)
-#                 st.success("Successfully registered your face!")
-#             except:
-#                 st.warning("Could not save the registered face. Please try again.")
-#             break
-#     cap.release()
+def capture_and_register():
+    global cap
+    cap = st.camera_input("Take a picture")
+    st.write("Hi, let's register your face")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.warning("Could not read from the camera. Please try again.")
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        st.image(frame, use_column_width=True, channels="RGB")
+        if st.button("Register"):
+            try:
+                cv2.imwrite("registered_face.jpg", frame)
+                st.success("Successfully registered your face!")
+            except:
+                st.warning("Could not save the registered face. Please try again.")
+            break
+    cap.release()
 
-# def detect():
-#     st.write("Face detection")
-#     registered_face = cv2.imread("registered_face.jpg")
-#     if registered_face is None:
-#         st.warning("No registered face found. Please register your face first.")
-#         return
-#     face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-#     cap = cv2.VideoCapture(0)
-#     while True:
-#         ret, frame = cap.read()
-#         if not ret:
-#             st.warning("Could not read from the camera. Please try again.")
-#             break
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-#         for (x, y, w, h) in faces:
-#             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#             roi_gray = gray[y:y + h, x:x + w]
-#             roi_color = frame[y:y + h, x:x + w]
-#             try:
-#                 res = cv2.matchTemplate(roi_gray, registered_face, cv2.TM_CCOEFF_NORMED)
-#                 if res > 0.8:
-#                     st.warning("Unlocked. Face recognized.")
-#                 else:
-#                     st.warning("Face not recognized.")
-#             except Exception as e:
-#                 st.warning("An error occurred while recognizing the face: {}".format(str(e)))
-#         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         st.image(frame, use_column_width=True, channels="RGB")
-#         if st.button("Stop"):
-#             break
-#     cap.release()
+def detect():
+    st.write("Face detection")
+    registered_face = cv2.imread("registered_face.jpg")
+    if registered_face is None:
+        st.warning("No registered face found. Please register your face first.")
+        return
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.warning("Could not read from the camera. Please try again.")
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+            try:
+                res = cv2.matchTemplate(roi_gray, registered_face, cv2.TM_CCOEFF_NORMED)
+                if res > 0.8:
+                    st.warning("Unlocked. Face recognized.")
+                else:
+                    st.warning("Face not recognized.")
+            except Exception as e:
+                st.warning("An error occurred while recognizing the face: {}".format(str(e)))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        st.image(frame, use_column_width=True, channels="RGB")
+        if st.button("Stop"):
+            break
+    cap.release()
 
-# if __name__ == '__main__':
-#     st.title("Face Recognition")
-#     st.sidebar.title("Menu")
-#     app_mode = st.sidebar.selectbox("Choose the app mode", ["Homepage", "Register", "Detect"])
+if __name__ == '__main__':
+    st.title("Face Recognition")
+    st.sidebar.title("Menu")
+    app_mode = st.sidebar.selectbox("Choose the app mode", ["Homepage", "Register", "Detect"])
 
-#     if app_mode == "Homepage":
-#         st.write("Welcome to Face Recognition")
-#         st.write("Please select a mode from the menu.")
-#     elif app_mode == "Register":
-#         capture_and_register()
+    if app_mode == "Homepage":
+        st.write("Welcome to Face Recognition")
+        st.write("Please select a mode from the menu.")
+    elif app_mode == "Register":
+        capture_and_register()
    
-import streamlit as st
 
-picture = st.camera_input("Take a picture")
